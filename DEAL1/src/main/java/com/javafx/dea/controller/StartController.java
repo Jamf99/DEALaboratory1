3ad8dd9091bcf23c949f3c32f36f03e35567ec66
Jamf99 <jamf_99@hotmail.com>
2018-08-17 12:08:39 -0500

    The class StartController has been created. This class contains the interface controllers

---
+++ DEAL1/src/main/java/com/javafx/dea/controller/StartController.java
@@ -0,0 +1,64 @@
+package com.javafx.dea.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class StartController {
+	
+    @FXML
+    private Button mergeButt1;
+
+    @FXML
+    private TextField arbitrarilyTxt;
+
+    @FXML
+    private TextField floatingPointtxt;
+
+    @FXML
+    private Button radixButt1;
+
+    @FXML
+    private Button heapButt1;
+
+    @FXML
+    private RadioButton arbitrarilyRadioButt;
+
+    @FXML
+    private RadioButton floatingPointRadioButt;
+
+    @FXML
+    private TextField lengthNumbersTxt;
+
+    @FXML
+    private TextField loweIntervalTxt;
+
+    @FXML
+    private RadioButton repeatedRadioButt;
+
+    @FXML
+    private RadioButton nonRepeatedRadioButt;
+
+    @FXML
+    private TextField upperIntervalTxt;
+
+    @FXML
+    private RadioButton orderedNormalyRadioButt;
+
+    @FXML
+    private RadioButton InverselyOrderedRadioButt;
+
+    @FXML
+    private RadioButton randomNumbersRadioButt;
+
+    @FXML
+    private Button heapButt2;
+
+    @FXML
+    private Button mergeButt2;
+
+    @FXML
+    private Button radixButt2;
+	
+}
