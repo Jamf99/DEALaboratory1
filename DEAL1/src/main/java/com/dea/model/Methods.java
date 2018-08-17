cff835fa04e9abc86401d3844f8cd6cc2ed2242f
kevinzarama <zaramaluna1999@hotmail.com>
2018-08-17 12:52:04 -0500

    this class contains the methods of sorting

---
+++ DEAL1/src/main/java/com/dea/model/Methods.java
@@ -0,0 +1,27 @@
+package com.dea.model;
+
+import java.util.ArrayList;
+
+public class Methods {
+	
+	public Methods() {
+		
+	}
+	
+	public ArrayList mergeSort(ArrayList numbers) {
+		return numbers;
+	}
+	
+	public ArrayList radixSort(ArrayList numbers) {
+		return numbers;
+	}
+	
+	public ArrayList heapSort(ArrayList numbers) {
+		return numbers;
+	}
+	
+}
+
+// merge
+// radix
+// heap
