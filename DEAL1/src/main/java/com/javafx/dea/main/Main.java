10beb4fed4598081c49053f663e41524688a828a
Jamf99 <jamf_99@hotmail.com>
2018-08-17 19:12:56 -0500

    An attribute was created for the main frame and instanced with the stage

--- DEAL1/src/main/java/com/javafx/dea/main/Main.java
+++ DEAL1/src/main/java/com/javafx/dea/main/Main.java
@@ -4,22 +4,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
+	private Stage window;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+		window = primaryStage;
+		window.setTitle("Algorithms and Data Estructures -- Laboratoy 1");
+		window.setResizable(false);
 		Parent root = FXMLLoader.load(getClass().getResource("/view/inicio.fxml"));
 		Scene scene = new Scene(root);
 		
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		window.setScene(scene);
+		window.show();
+	}
+	
+	public void showMessageResultMerge1() {
+		System.out.println("hola");;
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	
 	
 }
