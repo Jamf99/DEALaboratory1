a2ca3dad384cc925b7643d7ec589f2a06555c2cb
kevinzarama <zaramaluna1999@hotmail.com>
2018-08-17 20:06:52 -0500

    ..

--- DEAL1/src/main/java/com/dea/model/Methods.java
+++ DEAL1/src/main/java/com/dea/model/Methods.java
@@ -9,8 +9,21 @@ public class Methods {
 	}
 	
 	public ArrayList mergeSort(ArrayList numbers) {
-		
+		return merge(numbers, 0, numbers.size());
+	}
+	
+	public ArrayList merge(ArrayList numbers, int izq, int der) {
+		if(izq < der) {
+			int m = (izq + der) / 2;
+			merge(numbers, izq, m);
+			merge(numbers, m+1, der);
+			merges(numbers, izq, m, der);
+		}
 		return numbers;
+	}
+	
+	public void merges(ArrayList numbers, int izq, int m, int der) {
+		int i, j, k;
 	}
 	
 	public ArrayList radixSort(ArrayList numbers) {
