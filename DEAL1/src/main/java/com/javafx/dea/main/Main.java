b780394defa19360c5cdc152be87c9d57367395c
Jamf99 <jamf_99@hotmail.com>
2018-08-18 14:54:55 -0500

    I test the alert and information message in one of the buttons

--- DEAL1/src/main/java/com/javafx/dea/main/Main.java
+++ DEAL1/src/main/java/com/javafx/dea/main/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -21,10 +20,6 @@ public class Main extends Application{
 		
 		window.setScene(scene);
 		window.show();
-	}
-	
-	public void showMessageResultMerge1() {
-		System.out.println("hola");;
 	}
 	
 	public static void main(String[] args) {
