b64dfe08dcfa05f3f09ee6f1fa7f72b917837da0
Jamf99 <jamf_99@hotmail.com>
2018-08-19 11:45:14 -0500

    The test cases were generated to validate the operation of the ordering methods

---
+++ DEAL1/src/main/java/com/dea/tests/MethodsTest.java
@@ -0,0 +1,22 @@
+package com.dea.tests;
+
+import org.junit.jupiter.api.Test;
+
+import com.dea.model.Methods;
+
+import junit.framework.TestCase;
+
+class MethodsTest extends TestCase{
+	
+	private Methods methods;
+
+	private void stageOne() {
+		methods = new Methods();
+	}
+
+	@Test
+	void test() {
+		
+	}
+
+}
