5dafa7f4b4a148dac7784c1bca05fb7ed932472e
Jamf99 <jamf_99@hotmail.com>
2018-08-22 15:58:30 -0500

    Little changes in the class Methods

--- DEAL1/src/main/java/com/dea/model/Methods.java
+++ DEAL1/src/main/java/com/dea/model/Methods.java
@@ -9,36 +9,51 @@ public class Methods {
 		
 	}
 	
-	public void merge(int[] numeros) {
-		mergesort(numeros, 0, numeros.length-1);
-	}
-	
-	public static void mergesort(int A[],int izq, int der){
-	    if (izq<der){
-	            int m=(izq+der)/2;
-	            mergesort(A,izq, m);
-	            mergesort(A,m+1, der);
-	            merge(A,izq, m, der);
-	    }
-	}
-	
-	public static void merge(int A[],int izq, int m, int der){
-		   int i, j, k;
-		   int [] B = new int[A.length]; 
-		   for (i=izq; i<=der; i++) 
-		             B[i]=A[i];
+	public void mergeSort(double arr[], int l, int r) {
+        if (l < r) {
+            int m = (l+r)/2;
+            mergeSort(arr, l, m);
+            mergeSort(arr , m+1, r);
+            merge(arr, l, m, r);
+        }
+    }
+	public void merge(double arr[], int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
 
-		             i=izq; j=m+1; k=izq;
-		             while (i<=m && j<=der) 
-		             if (B[i]<=B[j])
-		                     A[k++]=B[i++];
-		             else
-		                     A[k++]=B[j++];
-		             while (i<=m) 
-		                           A[k++]=B[i++]; 
-	}
-	
-	public void radixSort(int[] numbers) {
+        double L[] = new double [n1];
+        double R[] = new double [n2];
+
+        for (int i=0; i<n1; ++i) {
+            L[i] = arr[l + i];
+        }
+        for (int j=0; j<n2; ++j) {
+            R[j] = arr[m + 1+ j];
+        }
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }	
+	public void radixSort(double[] numbers) {
 		int max    = 1;
         int nbytes = 4;
         int nColas = (int) Math.pow(2,nbytes) ;
@@ -46,10 +61,10 @@ public class Methods {
         for(int i=0; i<nColas; i++) cola[i]=new LinkedList<Integer>();
         int     div     = 0;
         for(int i=0; i<max; i++) {
-            for(int numero: numbers) {
-                if(i==0) if(numero>max) max=numero;
-                int numCola = (numero>>div) & 0xf;
-                cola[numCola].add(numero);
+            for(double numero: numbers) {
+                if(i==0) if(numero>max) max=(int)numero;
+                int numCola = ((int)numero>>div) & 0xf;
+                cola[numCola].add((int)numero);
             }
             div = div+nbytes;
             int j=0;
@@ -60,18 +75,20 @@ public class Methods {
         }
 	}
 	
-	public void heapSort(int[] numbers) {
+	public void heapSort(double[] numbers) {
 		final int N = numbers.length;
-        for(int nodo = N/2; nodo>=0; nodo--) heapsort(numbers, nodo, N-1);
+        for(int nodo = N/2; nodo>=0; nodo--) {
+        	heapsort(numbers, nodo, N-1);
+        }
         for(int nodo = N-1; nodo>=0; nodo--) {
-            int tmp = numbers[0];
+            double tmp = numbers[0];
             numbers[0]    = numbers[nodo];
             numbers[nodo] = tmp;
             heapsort(numbers, 0, nodo-1);
         }
 	}
 	
-	public void heapsort(int[] numbers, int nodo, int fin) {
+	public void heapsort(double[] numbers, int nodo, int fin) {
 		int izq = 2*nodo+1;
         int der = izq+1;
         int may;
@@ -79,7 +96,7 @@ public class Methods {
         if(der>fin) may=izq;
         else may= numbers[izq]>numbers[der]?izq:der;
         if(numbers[nodo] < numbers[may]) {
-            int tmp = numbers[nodo];
+            double tmp = numbers[nodo];
             numbers[nodo] = numbers[may];
             numbers[may]  = tmp;
             heapsort(numbers, may, fin);
