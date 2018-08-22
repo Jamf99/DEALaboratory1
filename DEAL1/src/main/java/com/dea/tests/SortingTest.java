d19df7770754c4995f71453d2f63324279165d55
Jamf99 <jamf_99@hotmail.com>
2018-08-22 15:59:48 -0500

    The class SortingTest has been updated

--- DEAL1/src/main/java/com/dea/tests/SortingTest.java
+++ DEAL1/src/main/java/com/dea/tests/SortingTest.java
@@ -22,31 +22,31 @@ class SortingTest extends TestCase{
 	@Test
 	public void testMerge() {
 		stage1();
-//		double[] n = {0,3,4,6,7.01,7.5,8,8,9,10,10,11,12,14,15};
-		int[] n = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-		s.merge((numbers2));
-		for(int i = 0; i < n.length; i++) {
-			assertEquals(n[i], numbers2[i]);
+		double[] numbers2 = {1,3,2,4,5,6,8,9};
+		double[] numbers = {1,2,4,6,5,3,9,8};
+		s.merge(numbers, (int) numbers[0], numbers.length-1);
+		for(int i = 0; i < numbers.length; i++) {
+			assertEquals(numbers[i], numbers2[i]);
 		}
 	}
 	
-	@Test
-	public void testRadix() {
-		stage1();
-		int[] n = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-		s.radix(numbers2);
-		assertArrayEquals(n, numbers2);
-	}
+//	@Test
+//	public void testRadix() {
+//		stage1();
+//		int[] n = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+////		s.radix(numbers2);
+//		assertArrayEquals(n, numbers2);
+//	}
 	
-	@Test
-	public void testHeap() {
-		stage1();
-//		double[] n = {0,3,4,6,7.01,7.5,8,8,9,10,10,11,12,14,15};
-		int[] n = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-		s.heap(numbers2);
-		for(int i = 0; i < n.length; i++) {
-			assertEquals(n[i], numbers2[i]);
-		}
-	}
+//	@Test
+//	public void testHeap() {
+//		stage1();
+////		double[] n = {0,3,4,6,7.01,7.5,8,8,9,10,10,11,12,14,15};
+//		int[] n = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+////		s.heap(numbers2);
+//		for(int i = 0; i < n.length; i++) {
+//			assertEquals(n[i], numbers2[i]);
+//		}
+//	}
 
 }
