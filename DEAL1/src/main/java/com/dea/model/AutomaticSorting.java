6108f432e2ee10f0e8e3a423c8ea2acc4fd8d9b3
Jamf99 <jamf_99@hotmail.com>
2018-08-23 09:16:42 -0500

    A method that generates random BigIntegers was created

--- DEAL1/src/main/java/com/dea/model/AutomaticSorting.java
+++ DEAL1/src/main/java/com/dea/model/AutomaticSorting.java
@@ -1,25 +1,28 @@
 package com.dea.model;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class AutomaticSorting extends Sorting{
 
-	public static final int ORDERED_NORMALLY = 1;
-	public static final int INVERSELY_ORDERED = 2;
-	public static final int ALEATORY_ORDERED = 3;
+	public static final int ORDERED_NORMALLY = 0;
+	public static final int INVERSELY_ORDERED = 1;
+	public static final int ALEATORY_ORDERED = 2;
 	
 	private boolean repeatedNumbers;
 	private BigInteger lowerInterval;
 	private BigInteger upperInterval;
+	private int lengthOfNumbers;
 	private int typeOfGeneratedNumbers;
 	private boolean percentOfDisorder;
 	
 	public AutomaticSorting(int typeOfNumbers, boolean repeatedNumbers, BigInteger lowerInterval,
-			BigInteger upperInterval, int typeOfGeneratedNumbers, boolean percentOfDisorder) {
+			BigInteger upperInterval, int lengthOfNumbers, int typeOfGeneratedNumbers, boolean percentOfDisorder) {
 		super(typeOfNumbers);
 		this.repeatedNumbers = repeatedNumbers;
 		this.lowerInterval = lowerInterval;
 		this.upperInterval = upperInterval;
+		this.lengthOfNumbers = lengthOfNumbers;
 		this.typeOfGeneratedNumbers = typeOfGeneratedNumbers;
 		this.percentOfDisorder = percentOfDisorder;
 	}
@@ -35,6 +38,10 @@ public class AutomaticSorting extends Sorting{
 	public BigInteger getUpperInterval() {
 		return upperInterval;
 	}
+	
+	public int getLengthOfNumbers() {
+		return lengthOfNumbers;
+	}
 
 	public int getTypeOfGeneratedNumbers() {
 		return typeOfGeneratedNumbers;
@@ -46,6 +53,28 @@ public class AutomaticSorting extends Sorting{
 	
 	public double calculatePercentOfDisorder() {
 		return 0;
+	}
+	
+	public BigInteger[] generatorBigInteger(double min, double max, int quantity) {
+		BigInteger[] numbers = new BigInteger[quantity];
+		int low = (int)(max-min)+1;
+		if(isRepeatedNumbers()) {
+			for(int i = 0; i < quantity; i++) {
+				numbers[i] = BigDecimal.valueOf(Math.random() * low + min).toBigInteger();
+			}
+		}else {
+			if(low >= quantity) {
+				for(int i = 0; i < quantity; i++) {
+					numbers[i] = BigDecimal.valueOf(Math.random() * low + min).toBigInteger();
+					for(int j = 0; j < i; j++) {
+						if(numbers[i] == numbers[j]) {
+							i--;
+						}
+					}
+				}
+			}
+		}
+		return numbers;
 	}
 
 
